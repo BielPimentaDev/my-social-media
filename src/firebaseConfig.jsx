40766dfd7d670b69f5f229be978a7ef5352accb9
Gabriel Pimenta <gabrielpimenttas@gmail.com>
2022-06-20 12:10:50 -0300

    fixing p problem from post

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -3,11 +3,13 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from '@firebase/firestore'
 import {getAuth} from 'firebase/auth'
 import {getStorage} from 'firebase/storage'
-import firebaseConfig from './env'
+import {firebaseConfig} from './env'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
